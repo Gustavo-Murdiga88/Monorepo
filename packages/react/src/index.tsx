1a8d81fc8624f112112133c35bcfd131e8fbcb69
Gustavo Murdiga <gumurdiga@gmail.com>
2022-10-20 00:27:07 -0300

    confirm

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,36 @@
-import { colors } from '@ignite-ui/tokens'
+import { styled } from './styles'
+import { ComponentProps } from 'react'
+import propTypes from 'prop-types'
 
-export function App() {
-  return <h1 style={{ color: colors.gray600 }}>Hello world </h1>
+export const Button = styled('button', {
+  border: 0,
+  backgroundColor: '$ignite300',
+  height: 30,
+  width: 120,
+  borderRadius: 5,
+  color: 'White',
+  fontFamily: '$default',
+
+  variants: {
+    size: {
+      lg: {
+        width: '240px',
+        height: '60',
+      },
+      small: {
+        width: '60px',
+        height: '15',
+      },
+    },
+  },
+  defaultVariants: {
+    size: 'lg',
+  },
+})
+
+Button.propTypes = {
+  size: propTypes.oneOf(['lg', 'small']),
+  color: propTypes.string.isRequired,
 }
+
+export type ButtonProps = ComponentProps<typeof Button>
