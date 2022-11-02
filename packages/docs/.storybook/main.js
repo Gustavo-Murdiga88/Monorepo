aca0925ee7a60b2e4be7957870de8195309a85d3
Gustavo Murdiga <gumurdiga@gmail.com>
2022-11-01 22:55:17 -0300

    fix: new deploy pages

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -18,7 +18,7 @@ module.exports = {
   }, 
   viteFinal: (config, {configType}) => {
     if(configType === 'PRODUCTION'){
-      config.base = '/Monorepo';
+      config.base = '/Monorepo/';
     }
     return config;
   }
